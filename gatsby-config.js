4f1d0591a434f7cee9cce2f0ddabdcc604e0ae31
Alessandro Capra <alessandro@quantoz.com>
2021-06-23 08:48:47 +0200

    Change website name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alessandro Capra`,
+    description: `Frontend developer // UX/UI designer // Done is better than perfect`,
+    author: `Alessandro Capra`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
